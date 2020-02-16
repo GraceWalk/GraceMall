96ee4168d826ffeb782702b0d5bf632ba719951f
GraceWalk <gracewalk1999@163.com>
2020-02-16 16:21:20 +0800

    import TabBar component
    Signed-off-by: GraceWalk <gracewalk1999@163.com>

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TabBar'
+  }
+</script>
+
+<style>
+  .tab-bar {
+    display: flex;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgb(231, 231, 231);
+    box-shadow: 0 -1px 3px rgba(204, 204, 204, 0.747)
+  }
+</style>
---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tab-bar-item" @click="skipPath">
+    <div v-if="!isActive"><slot name="item-img"></slot></div>
+    <div v-else><slot name="item-img-active"></slot></div>
+    <div :style="textStyle"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TabBarItem',
+    props: {
+      path: String,
+      activeColor: String
+    },
+    computed: {
+      isActive() {
+        return this.$route.path.indexOf(this.path) !== -1
+      },
+      textStyle() {
+        return this.isActive ? {color: this.activeColor} : {}
+      }
+    },
+    methods: {
+      skipPath() {
+        this.$router.replace(this.path)
+      }
+    }
+  }
+</script>
+
+<style>
+  .tab-bar-item {
+    flex: 1;
+    height: 49px;
+    text-align: center;
+    font-size: 14px;
+  }
+  .tab-bar-item img {
+    width: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+  }
+</style>
