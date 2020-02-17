6098fe8fb811e610472bcc9c5b6daced686653bc
GraceWalk <gracewalk1999@163.com>
2020-02-17 16:30:47 +0800

    add banners component
    Signed-off-by: GraceWalk <gracewalk1999@163.com>

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,43 @@
 <template>
-  <h1>首页</h1>
+  <div>
+    <!-- <nav-bar class="home-bar"><div class="center" slot="center">首页</div></nav-bar> -->
+    <home-swiper :banners="banners"/>
+  </div>
 </template>
 
 <script>
+  // import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './childComps/HomeSwiper'
+
+  import {getMultidata} from 'network/home'
+
   export default {
-    name: 'home'
+    name: 'home',
+    components: {
+      // NavBar,
+      HomeSwiper
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      getMultidata().then(res => {
+        this.banners = res.banner.list
+        this.recommends = res.recommend.list
+        console.log(this.banners)
+      })
+    }
   }
 </script>
 
-<style>
-
+<style scoped>
+.home-bar {
+  background-color: var(--color-tint)
+}
+.home-bar .center {
+  color: white
+}
 </style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,33 @@
+<template>
+  <swiper v-if="banners.length">
+    <swiper-item v-for="(item, key) in banners" :key="key">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+
+  export default {
+    name: 'HomeSwiper',
+    props: {
+      banners: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    components: {
+      Swiper,
+      SwiperItem
+    }
+  }
+</script>
+
+<style>
+
+</style>
