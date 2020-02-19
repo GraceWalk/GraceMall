8d71ba0cbc60936f6bc80da9a0478e59efafd2b5
GraceWalk <gracewalk1999@163.com>
2020-02-19 16:29:57 +0800

    add backTop component
    Signed-off-by: GraceWalk <gracewalk1999@163.com>

---
+++ src/components/content/backTop/BackTop.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="~assets/img/common/top.png" alt="">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BackTop'
+  }
+</script>
+
+<style scoped>
+ img {
+   width: 43px;
+   height: 43px;
+ }
+</style>
