29a20a664cc1bca42942743a3fc604a40d818959
GraceWalk <gracewalk1999@163.com>
2020-02-22 10:16:46 +0800

    add DetailSwiper component in Detail
    Signed-off-by: GraceWalk <gracewalk1999@163.com>

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <keep-alive exclude="detail"><router-view/></keep-alive>
+    <keep-alive exclude="Detail"><router-view/></keep-alive>
     <main-tab-bar/>
   </div>
 </template>
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,33 +1,41 @@
 <template>
   <div>
-    <detail-nav-bar/>
+    <detail-nav-bar class="nav-bar"/>
+    <detail-swiper :topImages="topImages"/>
   </div>
 </template>
 
 <script>
   import DetailNavBar from './childComps/DetailNavBar'
+  import DetailSwiper from './childComps/DetailSwiper'
 
   import {getDetailData} from 'network/detail'
 
   export default {
     name: 'Detail',
     components: {
-      DetailNavBar
+      DetailNavBar,
+      DetailSwiper
     },
     data() {
       return {
-        iid: ''
+        iid: '',
+        topImages: []
       }
     },
     created() {
       this.iid = this.$route.query.iid
       getDetailData(this.iid).then(res => {
-        console.log(res.data)
+        const data = res.data.result
+        this.topImages = data.itemInfo.topImages
       })
     }
   }
 </script>
 
 <style scoped>
-
+.nav-bar {
+  z-index: 2;
+  background-color: #fff;
+}
 </style>
